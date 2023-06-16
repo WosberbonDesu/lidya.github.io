f4c84085ba6f35617b6298c7615b4d742bb42757
Berke Can Pınar <69467096+WosberbonDesu@users.noreply.github.com>
2023-06-16 12:11:58 +0300

    Create const.js
    hyj

---
+++ const.js
@@ -0,0 +1,22 @@
+
+function fun1(a) {
+    fun = function fun2(b) {
+        return a + b;
+    }
+    return fun;
+}
+function GFG_Fun() {
+    console.log(fun1("This is ")("GeeksforGeeks"));
+}
+GFG_Fun()
+
+function fun1(a) {
+    fun = function fun2(b) {
+        return a + b;
+    }
+    return fun;
+}
+function GFG_Fun() {
+    console.log(fun1("This is ")("GeeksforGeeks"));
+}
+GFG_Fun()
